f1c6829dbc1d6994e236ee45cd9a524b39b72c87
Najmul H. Bappy <http.bappy@gmail.com>
2021-07-22 10:19:04 +0600

    finalize initial django channels with basic settings and final chat version

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,3 +116,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "mysite.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    },
+}
